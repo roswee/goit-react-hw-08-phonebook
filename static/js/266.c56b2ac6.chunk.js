49d005461248a4509e83b1fe9d9065ee3fe5ffd3
roswee <pawel94roslaniec@gmail.com>
2022-12-27 15:40:52 +0000

    Deploying to gh-pages from @ roswee/goit-react-hw-08-phonebook@b59327a8f085caabfd529794dfce48a41c787cd8 🚀

---
+++ static/js/266.c56b2ac6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{266:function(e,t,o){o.r(t),o.d(t,{default:function(){return n}});var r=o(184);function n(){return(0,r.jsx)("div",{className:"position-absolute top-50 start-50 translate-middle text-white",children:(0,r.jsx)("h1",{children:"Welcome on my app to collect your contacts. Register or log in."})})}}}]);
+//# sourceMappingURL=266.c56b2ac6.chunk.js.map
