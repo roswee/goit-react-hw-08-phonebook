5510f9f4eab5ba742881d5752175557c6e6d0daf
Paweł Rosłaniec <pawel94roslaniec@gmail.com>
2023-01-02 20:13:02 +0100

    bugfix/redirect in private route

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,7 +1,7 @@
 import { useAuth } from 'hooks';
 import { Navigate } from 'react-router-dom';
 
-export const PrivateRoute = ({ component: Component, redirectTo }) => {
+export const PrivateRoute = ({ component: Component, redirectTo = '/login' }) => {
   const { isLoggedIn, isRefreshing } = useAuth();
   const shouldRedirect = !isRefreshing && !isLoggedIn;
   return shouldRedirect ? <Navigate to={redirectTo} /> : <Component />;
